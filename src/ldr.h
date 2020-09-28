c3e7547913fed99ad6d44db31c1d1d0a8ddfa627
poweredbypie <67135060+poweredbypie@users.noreply.github.com>
2020-09-27 20:22:02 -0700

    remove unnecessary macros

--- src/ldr.h
+++ src/ldr.h
@@ -7,7 +7,6 @@
 #define BTN_CALLBACK(__fcnName) void __stdcall __fcnName(void* pSender)
 #define COCOS_HOOK __fastcall
 #define LIST_LENGTH 10
-#define GET_LENGTH(__list) __list.length = (__list.strings.size() - __list.index) < LIST_LENGTH ? (__list.strings.size() - __list.index) : LIST_LENGTH;
 
 namespace ldr {
 	namespace vars {
